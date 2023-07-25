6ad0351184e74f0ff091bb1d39ce2f279e07fc04
KCM <knightedcodemonkey@gmail.com>
2023-07-25 15:50:00 -0500

    feat: mapper.

--- src/format.js
+++ src/format.js
@@ -1,20 +1,44 @@
 import { simple } from 'acorn-walk'
 import MagicString from 'magic-string'
+import { dump } from '@knighted/dump'
 
-const update = (sourceNode, code, callback) => {
+const getUpdateFromMap = (map, value) => {
+  /**
+   * This should be run in insertion order of the original
+   * map keys. The array was built with Object.entries()
+   * which uses the same ordering as for...in.
+   * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in#description
+   */
+  for (const [regexp, sub] of map) {
+    if (regexp.test(value)) {
+      return value.replace(regexp, sub)
+    }
+  }
+}
+const getUpdated = (resolver, opts, specifier) => {
+  if (typeof resolver === 'function') {
+    return resolver({ ...opts, value: specifier })
+  }
+
+  if (Array.isArray(resolver)) {
+    return getUpdateFromMap(resolver, specifier)
+  }
+}
+const update = (sourceNode, code, callbackOrMap) => {
+  dump.log(sourceNode)
   const { type, start, end, loc, value } = sourceNode
-  const updated = callback({ type, start, end, loc, value })
+  const updated = getUpdated(callbackOrMap, { type, start, end, loc }, value)
 
   if (typeof updated === 'string') {
     code.update(start + 1, end - 1, updated)
   }
 }
-const format = (file, ast, callback) => {
+const format = (file, ast, callbackOrMap) => {
   const code = new MagicString(file)
 
   simple(ast, {
     ImportDeclaration(node) {
-      update(node.source, code, callback)
+      update(node.source, code, callbackOrMap)
     },
     ImportExpression(node) {
       const { source } = node
@@ -22,7 +46,7 @@ const format = (file, ast, callback) => {
 
       switch (source.type) {
         case 'Literal': {
-          update(source, code, callback)
+          update(source, code, callbackOrMap)
           break
         }
         case 'NewExpression': {
@@ -30,7 +54,7 @@ const format = (file, ast, callback) => {
 
           if (source.callee.type === 'Identifier' && name === 'String') {
             const value = file.slice(source.start, source.end)
-            const updated = callback({ type, start, end, loc, value })
+            const updated = getUpdated(callbackOrMap, { type, start, end, loc }, value)
 
             if (typeof updated === 'string') {
               // Should provide "raw" updated value
@@ -41,7 +65,7 @@ const format = (file, ast, callback) => {
         }
         case 'BinaryExpression': {
           const value = file.slice(source.start, source.end)
-          const updated = callback({ type, start, end, loc, value })
+          const updated = getUpdated(callbackOrMap, { type, start, end, loc }, value)
 
           if (typeof updated === 'string') {
             // Should provide "raw" updated value
@@ -51,7 +75,7 @@ const format = (file, ast, callback) => {
         }
         case 'TemplateLiteral': {
           const value = file.slice(source.start + 1, source.end - 1)
-          const updated = callback({ type, start, end, loc, value })
+          const updated = getUpdated(callbackOrMap, { type, start, end, loc }, value)
 
           if (typeof updated === 'string') {
             code.update(start + 1, end - 1, updated)
@@ -64,11 +88,11 @@ const format = (file, ast, callback) => {
       const { source } = node
 
       if (source) {
-        update(source, code, callback)
+        update(source, code, callbackOrMap)
       }
     },
     ExportAllDeclaration(node) {
-      update(node.source, code, callback)
+      update(node.source, code, callbackOrMap)
     },
   })
 
---
+++ test/mapper.js
@@ -0,0 +1,56 @@
+import { describe, it } from 'node:test'
+import assert from 'node:assert/strict'
+import { fileURLToPath } from 'node:url'
+import { dirname, resolve, join } from 'node:path'
+
+import { specifier } from '../src/index.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const fixtures = resolve(__dirname, '__fixtures__')
+const { mapper } = specifier
+
+describe('mapper', () => {
+  it('updates specifiers in a file given a map of regex to new value', async () => {
+    const code = await mapper(join(fixtures, 'mapper.js'), {
+      foo: 'bar',
+      '(.+)\\${.+}(.+)?': '$1string$2',
+      '^new String\\((.+)\\)$': '$1',
+      "'(.+)'\\s*\\+\\s*'(.+)'": '"./be.js"',
+      '([^.]+)\\.js$': '$1.cjs',
+    })
+
+    assert.ok(code.indexOf('bar') > -1)
+    assert.ok(code.indexOf('./module.cjs') > -1)
+    assert.ok(code.indexOf('123.cjs') > -1)
+    assert.ok(code.indexOf('./not-matched.mjs') > -1)
+    assert.ok(code.indexOf('import(`./template/string.js`') > -1)
+    // Binary expressions and new expressions need to return the "raw" value, i.e. including any quotes.
+    assert.ok(code.indexOf('import("./be.js")') > -1)
+    assert.ok(code.indexOf("import('./ctor.cjs')") > -1)
+  })
+
+  it('returns an object when errors happen', async () => {
+    let ret = await mapper('/foo')
+
+    assert.equal(ret.error, true)
+    assert.equal(ret.msg, 'The provided path /foo does not resolve to a file on disk.')
+
+    ret = await mapper('test/__fixtures__')
+
+    assert.equal(ret.error, true)
+    assert.equal(ret.msg, 'The provided path test/__fixtures__ is not a file.')
+
+    ret = await mapper('test/__fixtures__/syntaxError.js')
+
+    assert.equal(ret.error, true)
+    assert.equal(ret.syntaxError, true)
+
+    ret = await mapper('test/__fixtures__/mapper.js', {
+      '(': './invalid-regex.js',
+    })
+
+    assert.equal(ret.error, true)
+    assert.ok(ret.msg.indexOf('Invalid regular expression') > -1)
+  })
+})
