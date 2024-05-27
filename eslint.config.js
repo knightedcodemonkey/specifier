e1c1d79a3694c3bacefd6c8369b0736d89d90789
KCM <knightedcodemonkey@gmail.com>
2024-05-27 14:38:53 -0500

    chore!: upgrade eslint. BREAKING CHANGE: upgrade supported Node.js versions to >= 20.0.0.

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import nodePlugin from 'eslint-plugin-n'
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  nodePlugin.configs['flat/recommended'],
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      'no-console': 'error',
+    },
+  },
+  {
+    files: ['test/__fixtures__/**/*'],
+    rules: {
+      'no-undef': 'off',
+      'n/no-missing-import': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  },
+)
